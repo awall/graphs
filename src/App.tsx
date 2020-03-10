b8bf1ce9abcfa24a914ec17003a2a1c4f810afe6
Adam Wall <Adam.Wall@Aucerna.com>
2020-03-10 10:58:47 -0600

    Initial skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router
+, Switch
+, Route
+, Link
+} from "react-router-dom";
+
 import './App.css';
+import BizCharts from "./BizCharts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default () => 
+  (<Router>
+    <header>
+      <Link to="/bizcharts">BizCharts</Link>
+      <Link to="/plottable">Plottable</Link>
+      <Link to="/react-svg-d3">React SVG + D3</Link>
+    </header>
 
-export default App;
+    <Switch>
+      <Route path="/bizcharts">
+          <BizCharts />
+      </Route>
+      <Route path="/plottable">
+          <div />
+      </Route>
+      <Route path="/react-svg-d3">
+          <div />
+      </Route>  
+    </Switch>
+      
+  </Router>)
